37152ec075862174fb07b07d233ee6f1dfb17043
jrjrspnl <jerwinespenile@gmail.com>
2025-07-29 21:10:44 +0800

    Map through movie lsit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import MovieCard from "../components/MovieCard";
+const movies = [
+  { id: 1, title: "John Wick", release_date: "2020" },
+  { id: 2, title: "Terminator", release_date: "1999" },
+  { id: 3, title: "The Matrix", release_date: "1998" },
+];
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <div className="movies-grid">
+        {movies.map((movie) => {
+          <MovieCard movie={movie} key={movie.id} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
