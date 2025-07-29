f835bbf757a6ddbf813ee64cfb0c6b8f0fde3391
jrjrspnl <jerwinespenile@gmail.com>
2025-07-29 22:49:25 +0800

    Added some components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Favorites from "./pages/Favorites";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div>
-      <Home />
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
+        </Routes>
+      </main>
     </div>
   );
 };
