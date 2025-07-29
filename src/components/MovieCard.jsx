84a0f9eaa3a0e8ac92efcec89788452aea6bb22c
jrjrspnl <jerwinespenile@gmail.com>
2025-07-29 21:12:06 +0800

    Add MovieCard with image, title, data, and  button

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-const MovieCard = () => {
-  return <div>MovieCard</div>;
+const MovieCard = ({ movie }) => {
+  const onFavoriteClick = () => {
+    alert("clicked");
+  };
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img src={movie.url} alt={movie.title} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={onFavoriteClick}>
+            ❤
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date}</p>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
