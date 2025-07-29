c3a467792431b6312b99afcf0feb9324167a128f
jrjrspnl <jerwinespenile@gmail.com>
2025-07-29 22:49:39 +0800

    Put some text in Favorite

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Favorites = () => {
-  return <div>Favorites</div>;
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here</p>
+    </div>
+  );
 };
 
 export default Favorites;
