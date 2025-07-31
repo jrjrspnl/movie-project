d3659c5944f5e5ee679d6bcfb28b598eeb7a9a3e
jrjrspnl <jerwinespenile@gmail.com>
2025-07-31 22:47:50 +0800

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import React from "react";
 import "./css/App.css";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
-import Favorites from "./pages/Favorites";
-import Navbar from "./components/Navbar";
-const App = () => {
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
+
+function App() {
   return (
-    <div>
-      <Navbar />
+    <MovieProvider>
+      <NavBar />
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/favorites" element={<Favorites />}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   );
-};
+}
 
 export default App;
